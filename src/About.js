956f16f59605d18d11a879a1cc7f530e74ce9181
Arnavya <arnavyachettri@gmail.com>
2024-06-16 23:21:24 +0530

    images loading

--- src/About.js
+++ src/About.js
@@ -7,10 +7,12 @@ const About = () => {
       <h2>About</h2>
       <div id="para_img">
         <div>
-        <p class="size">This is where you can describe about yourself. The more you describe about yourself, the better it is!</p>
+        <p class="size">My name is Arnavya Chettri, and I am passionate about my work. Coding, building, and sports are my primary hobbies, and they each play a significant role in shaping my approach to life and work. </p>
       <p class="size">
-        Extra Information about you, like hobbies and your goals.
-        Example Paragraph: I am passionate about my work. Because I love what I do, I have a steady source of motivation that drives me to do my best. In my last job, this passion led me to challenge myself daily and learn new skills that helped me to do better work. For example, I taught myself how to use Photoshop to improve the quality of our photos and graphics. I soon became the go-to person for any design needs.
+      Because I love what I do, I have a steady source of motivation that drives me to do my best. This passion has led me to challenge myself daily and continuously learn new skills, which has helped me improve my work quality.
+
+I love to read books, not to get something out of it but to get a different perspective to see things. This habit has enriched my creativity and problem-solving skills, making me more adaptable and innovative in my professional endeavors.
+
       </p>
         </div>
      
--- src/Skills.js
+++ src/Skills.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Skills.css';
+import { FaJava, FaJs, FaPhp, FaPython , FaAndroid, FaReact, FaNodeJs, FaGitAlt, FaDocker} from 'react-icons/fa';
+import { SiMysql , SiAndroidstudio} from 'react-icons/si';
 
 function Skills() {
   return (
@@ -10,29 +12,40 @@ function Skills() {
       <div className="skills-category">
         <h3>Languages & Databases</h3>
         <div className="skills-icons">
-          <img src="path/to/java-logo.png" alt="Java" />
-          <img src="path/to/php-logo.png" alt="PHP" />
-          <img src="path/to/js-logo.png" alt="JavaScript" />
-          <img src="path/to/python-logo.png" alt="Python" />
-          <img src="path/to/mysql-logo.png" alt="MySQL" />
+          {/* <img src="/Users/arnavya/Desktop/VS_Code/React/portflio2/src/javalogo.png" alt="Java" />
+          <img src="/Users/arnavya/Desktop/VS_Code/React/portflio2/src/phplogo.png" alt="PHP" />
+          <img src="/Users/arnavya/Desktop/VS_Code/React/portflio2/src/jslogo.png" alt="JavaScript" />
+          <img src="/Users/arnavya/Desktop/VS_Code/React/portflio2/src/pythonlogo.png" alt="Python" />
+          <img src="/Users/arnavya/Desktop/VS_Code/React/portflio2/src/mysqllogo.pngl" alt="MySQL" /> */}
+        <FaJava className="icon" />
+        <FaJs className="icon" />
+        <FaPhp className="icon" />
+        <SiMysql className="icon" />
+        <FaPython className="icon" />
         </div>
       </div>
 
       <div className="skills-category">
         <h3>Frameworks & Technologies</h3>
         <div className="skills-icons">
-          <img src="path/to/android-logo.png" alt="Android" />
+          {/* <img src="path/to/android-logo.png" alt="Android" />
           <img src="path/to/react-logo.png" alt="React" />
-          <img src="path/to/nodejs-logo.png" alt="Node.js" />
+          <img src="path/to/nodejs-logo.png" alt="Node.js" /> */}
+           <FaAndroid className="icon" />
+      <FaReact className="icon" />
+      <FaNodeJs className="icon" />
         </div>
       </div>
 
       <div className="skills-category">
         <h3>Tools & Platforms</h3>
         <div className="skills-icons">
-          <img src="path/to/android-studio-logo.png" alt="Android Studio" />
+          {/* <img src="path/to/android-studio-logo.png" alt="Android Studio" />
           <img src="path/to/git-logo.png" alt="Git" />
-          <img src="path/to/docker-logo.png" alt="Docker" />
+          <img src="path/to/docker-logo.png" alt="Docker" /> */}
+          <SiAndroidstudio className="icon" />
+      <FaGitAlt className="icon" />
+      <FaDocker className="icon" />
         </div>
       </div>
     </div>
